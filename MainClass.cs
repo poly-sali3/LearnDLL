7482916bdfbfe5d0f23bd942604dd33cac98504e
MSI <MSI@HOME-PC>
2025-01-08 14:51:19 +0300

    Добавьте файлы проекта.

---
+++ MainClass.cs
@@ -0,0 +1,28 @@
+﻿namespace LearnDLL
+{
+    public class MainClass
+    {
+        private int _value;
+
+        
+        public MainClass(int value)
+        {
+            if (value < 0 || value > 255)
+            {
+                throw new Exception( "Значение должно быть от 0 до 255");
+            }
+            _value = value;
+        }
+
+        
+        public int GetDifference(int inputValue)
+        {
+            if (inputValue < 0 || inputValue > 255)
+            {
+                return 0;
+            }
+            return Math.Abs(inputValue - _value);
+        }
+
+    }
+}
